c5c633067e0ea1d4b99b54d601f435703f8b66fe
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-07-02 14:04:42 -0300

    fix - Polimento do layout e adição do código js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",() => {
+
+    const homebox = document.getElementById('home-ca')
+    /* BTN === Home Section*/
+    const btnone = document.getElementById('btn-andromeda');
+    const btntwo = document.getElementById('btn-athena');
+    const btnthree = document.getElementById('btn-cancer');
+    const btnfour = document.getElementById('btn-sorent');
+});

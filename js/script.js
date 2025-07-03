b229c58eb46dc83489ce9c8a903914decbf515de
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-07-03 13:56:40 -0300

    fix - Alterações no home HTML e no CSS

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
 document.addEventListener("DOMContentLoaded",() => {
 
-    const homebox = document.getElementById('home-ca')
-    /* BTN === Home Section*/
-    const btnone = document.getElementById('btn-andromeda');
-    const btntwo = document.getElementById('btn-athena');
-    const btnthree = document.getElementById('btn-cancer');
-    const btnfour = document.getElementById('btn-sorent');
+    $('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 10,
+    nav: false,
+    center: true,
+    items:1,
+    dotsEach: true,
+});
+
+    /*$('.owl-carousel').find('.carousel-item').each(function() {
+    var imgContainer = $(this),
+        bkImg = imgContainer.find('img').attr('src');
+    imgContainer.css("background-image", 'url("' + bkImg + '")');
+    });*/
+
+        $('.owl-carousel').each(function() {
+        //Find each set of dots in this carousel
+    $(this).find('.owl-dot').each(function(index) {
+        //Add one to index so it starts from 1
+        $(this).attr('aria-label', index + 1);
+    });
+    });
+
+
 });

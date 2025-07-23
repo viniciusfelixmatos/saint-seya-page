1fae4454ce72c1c3d2377855a4d7528bff5edd85
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-07-22 21:15:56 -0300

    feat: Adição de novas áreas e aprimoramento do layout

--- js/script.js
+++ js/script.js
@@ -26,6 +26,21 @@ document.getElementById('btn-menu').addEventListener('click', function() {
     }
 });
 
+$('.characters-carousel').owlCarousel({
+    loop: false,
+    center: false,
+    margin: 10,
+    dots: false,
+    touchDrag: true,
+    mouseDrag: true,
+    responsive:{
+        0:{ items:1 },
+        600:{ items:1 },
+        1000:{ items:6 }
+    }
+});
+
+
 const buttonsowl = document.querySelectorAll(".owl-dot span");
     let i = 0;
     for (i; i < buttonsowl.length; i++) {

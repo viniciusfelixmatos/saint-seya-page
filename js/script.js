5fe10063338e89cdab472265b56dbd549b12fd4e
Vinicius Matos <163915966+viniciusfelixmatos@users.noreply.github.com>
2025-07-30 10:45:01 -0300

    feat: Aprimoramento e adição de novas áreas no layout

--- js/script.js
+++ js/script.js
@@ -1,67 +1,59 @@
-document.getElementById('btn-menu').addEventListener('click', function() {
+document.getElementById('btn-menu').addEventListener('click', function () {
     this.classList.toggle('active');
-
-    // Alterna visibilidade do menu
     const menu = document.getElementById('nav-list');
     menu.classList.toggle('show');
-  });  
+});
 
-    $('.home-carousel').owlCarousel({
-    loop:true,
+function numberHomeDots(event) {
+    const carousel = event.target;
+    const dots = carousel.querySelectorAll(".owl-dot span");
+    dots.forEach((dot, index) => {
+        dot.textContent = index + 1;
+    });
+}
+
+$('.home-carousel').owlCarousel({
+    loop: true,
     center: true,
-    margin:10,
+    margin: 10,
     dots: true,
     touchDrag: false,
     mouseDrag: false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        }
-    }
+    responsive: {
+        0: { items: 1 },
+        600: { items: 1 },
+        1000: { items: 1 }
+    },
+    onInitialized: numberHomeDots
 });
 
 $('.characters-carousel').owlCarousel({
     loop: false,
-    center: true,
+    center: false,
     margin: 20,
     dots: false,
-    autoWidth:true,
+    autoWidth: true,
     touchDrag: true,
     mouseDrag: true,
     responsive: {
-        320: { items: 2},
-        600:{ items:3 },
-        1000:{ items:6 }
-    }
+        320: { items: 2 },
+        600: { items: 3 },
+        1000: { items: 6 }
+  }
 });
 
 $('.houses-carousel').owlCarousel({
     loop: false,
     center: true,
     margin: 20,
-    dots: false,
+    dots: true,
     nav: true,
-    navText: [$('.am-next'),$('.am-prev')],
-    autoWidth:true,
+    navText: [$('.am-next'), $('.am-prev')],
+    autoWidth: true,
     touchDrag: true,
     mouseDrag: true,
     responsive: {
-        1000:{ items:1 }
+        1000: { items: 1 }
     }
 });
-
-
-const buttonsowl = document.querySelectorAll(".owl-dot span");
-    let i = 0;
-    for (i; i < buttonsowl.length; i++) {
-        console.log(i)
-        console.log(buttonsowl[i])
-        buttonsowl[i].innerHTML = i+1;
-    };
 
